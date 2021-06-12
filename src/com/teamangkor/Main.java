10d0ceca306cc90702119e8a05664e3a7d40b3c1
vattcarter7 <vattcarter@gmail.com>
2021-06-12 16:02:11 +0700

    java primitive types

--- src/com/teamangkor/Main.java
+++ src/com/teamangkor/Main.java
@@ -3,6 +3,10 @@ package com.teamangkor;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A'; // char must use with single quote
+        boolean isElligible = false;
     }
 }
